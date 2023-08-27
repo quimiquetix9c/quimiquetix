e300984ffe020a3b03692dac958c392bfbfa0400
quimiquetix9c <haikou590@gmail.com>
2023-08-27 19:58:58 -0300

    tabela finalizada - inicio jogo

---
+++ scripts/jogo.js
@@ -0,0 +1,12 @@
+const images = [
+    "aluminio-par1",
+    "aluminio-par2",
+    "calcio-par1",
+    "calcio-par2",
+    "ferro-par1",
+    "ferro-par2",
+    "oxigenio-par1",
+    "oxigenio-par2",
+    "sodio-par1",
+    "sodio-par2"
+]
--- scripts/tabela.js
+++ scripts/tabela.js
@@ -17,15 +17,18 @@ PopulaDadosTabela(0, 15, tabela2, 56)
 PopulaDadosTabela(15, 15, tabela2, 73)
 
 
-
+DestacaLegenda()
 
 
 function CriaGridTabela(quantidade, classe, tabela) {
     for(let i=0;i<quantidade;i++) {
         let elemento = document.createElement("div")
         elemento.className = classe
-        elemento.addEventListener("click", () => {
-            alert("alo")
+        elemento.addEventListener("click", (event) => {
+            var numeroAtomico = event.target.parentNode.dataset.numatom
+            if (!numeroAtomico) return;
+
+            DadosdoElemento(numeroAtomico)
         })
         tabela.appendChild(elemento)
     }
@@ -39,20 +42,23 @@ function PopulaDadosTabela(inicio, quantidade, tabela, offset=0) {
         //Lantanídeos
         if (posGridAtual == 56) {
             area[i+inicio].innerHTML = `
-            <p class="lantanideos">* 57-71</p>
+            <div class="container-elemento lantanideos">
+                <p>* 57-71</p>
+            </div>   
             
             `
         }
         //actinideos
         else if (posGridAtual == 74){
             area[i+inicio].innerHTML = `
-            <p class="actinideos">* 89-103</p>
-            
+            <div class="container-elemento actinideos">
+                <p>* 89-103</p>
+            </div>   
             `
         }
         else {
             area[posGridAtual].innerHTML = `
-            <div class="container-elemento ${dados[posElementoAtual].classificacao}">
+            <div class="container-elemento ${dados[posElementoAtual].classificacao} ${dados[posElementoAtual].classificacao}-container" data-numatom="${dados[posElementoAtual].atomicNumber}">
                 <p class="numAtomico">${dados[posElementoAtual].atomicNumber}</p>
                 <p class="sigla">${dados[posElementoAtual].symbol}</p>
                 <p class="nome">${dados[posElementoAtual].name}</p>
@@ -61,4 +67,69 @@ function PopulaDadosTabela(inicio, quantidade, tabela, offset=0) {
             `
         }
     }
+}
+
+function DadosdoElemento(numAtomico) {
+    let dadosElemento = dados.find((obj) => obj.atomicNumber == numAtomico);
+    let containerInfo = document.querySelector(".container-campos")
+
+    containerInfo.querySelector("#info-nome").textContent = dadosElemento.name
+    containerInfo.querySelector("#info-numatom").textContent = dadosElemento.atomicNumber
+    containerInfo.querySelector("#info-massa").textContent = dadosElemento.weight
+    containerInfo.querySelector("#info-familia").textContent = dadosElemento.familia
+    containerInfo.querySelector("#info-periodo").textContent = dadosElemento.periodo
+    containerInfo.querySelector("#info-pontoeb").textContent = dadosElemento.pontoEbulicao
+    containerInfo.querySelector("#info-pontofus").textContent = dadosElemento.pontoFusao
+
+
+
+}
+
+function DestacaLegenda() {
+    let legendas = document.querySelectorAll(".box-legenda > div")
+    let legendaAtiva = null 
+
+    legendas.forEach((legenda) => {
+        legenda.addEventListener("click", () => {
+            if (legendaAtiva?.id === legenda.id) {
+                legendaAtiva = null
+                LimpaSelecao()
+                LimpaLegendasAtivas()
+            } else {
+                LimpaLegendasAtivas();
+                legendaAtiva = legenda
+                legenda.classList.add("ativo")
+                AlteraVisubilidadeLegenda(legenda.id)
+            }        
+        })
+    })
+}
+
+function AlteraVisubilidadeLegenda(tipoElemento) {
+    document.querySelectorAll(".container-elemento").forEach((elemento) => {
+
+        if (elemento.classList.contains(tipoElemento)) {
+            elemento.classList.add("destaque")
+            elemento.classList.remove("nao-destaque")
+
+        } else {
+            elemento.classList.remove("destaque")
+            elemento.classList.add("nao-destaque")
+        }
+    })
+}
+
+function LimpaSelecao() {
+    document.querySelectorAll(".container-elemento").forEach((elemento) => {
+        elemento.classList.remove("destaque")
+        elemento.classList.remove("nao-destaque")
+
+    })
+}
+
+function LimpaLegendasAtivas() {
+    let legendas = document.querySelectorAll(".box-legenda > div")
+    legendas.forEach((legenda) => {
+        legenda.classList.remove("ativo")
+    })
 }
